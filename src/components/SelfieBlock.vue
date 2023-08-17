c858eab046cf698a8d33a7902b5a85618ba83c92
Aleksey <direct.abu@yandex.ru>
2023-08-17 19:13:16 +0300

    init

---
+++ src/components/SelfieBlock.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="selfie">
+    <div v-for="(item, index) in self" :key="index + item.id" class="selfie__block">
+      <span class="selfie__block__title">{{item.desc}}</span>
+      <img :src="'/static/' + item.url" class="selfie__block__img" :alt="item.desc">
+    </div>
+  </div>
+</template>
+
+<script>
+export default ({
+  data: () => ({
+    self: [{ desc: 'Фото со школы', url: 'high_school_avatar.jpg', id: '1' }, { desc: 'Актуальный аватар', url: 'today-avatar.jpg', id: '2' }],
+  }),
+});
+</script>
+
+<style lang="scss" scoped>
+.selfie {
+    margin: 40px auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  &__block {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+      &:first-child {
+          margin-right: 40px;
+      }
+    &__title {
+      font-size: 16px;
+    }
+    &__img {
+      width: 200px;
+      height: 150px;
+      object-fit: cover;
+    }
+  }
+}
+</style>
